c9eb23d8aa0335e9509115ee1bc39445795bc74e
ZScurlock <54798255+ZScurlock@users.noreply.github.com>
2023-02-20 17:22:03 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 #from distutils.core import setup # Finish this
 setup(
-  name = 'snp_haplotype',         # How you named your package folder (MyLib)
-  packages = ['snp_haplotype'],   # Chose the same as "name"
+  name = 'snp_haplotype',         
+  packages = ['snp_haplotype'],  
   version = '0.1',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'Finds SNP haplotypes between samples and produces a Minimum Spanning Tree showing their relationship',   # Give a short description about your library
-  author = 'Zac Scurlock',                   # Type in your name
-  author_email = 'zac.scur@hotmail.com',      # Type in your E-Mail
-  url = 'https://github.com/ZScurlock/SNP_haplotype',   # Provide either the link to your github or to your website
+  license='MIT',        
+  description = 'Finds SNP haplotypes between samples and produces a Minimum Spanning Tree showing their relationship',
+  author = 'Zac Scurlock',                   
+  author_email = 'zac.scur@hotmail.com',      
+  url = 'https://github.com/ZScurlock/SNP_haplotype',  
   download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['SNP', 'Haplotype', 'Haplotype network', 'Minimum spanning tree'],   # Keywords that define your package best
+  keywords = ['SNP', 'Haplotype', 'Haplotype network', 'Minimum spanning tree'],
   install_requires=[            
           'pandas',
           'numpy',
@@ -20,12 +20,9 @@ setup(
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Bioinformaticians',      # Define that your audience are developers
+    'Intended Audience :: Bioinformaticians',      
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6.9',
   ],
 )
